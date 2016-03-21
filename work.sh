114fd4c8fa9c591fb4cd9c7d6e47b1724f903e53
ppaul <alpcppaul@gmail.com>
2016-03-21 12:40:27 +0800

    1.03

--- work.sh
+++ work.sh
@@ -1,8 +1,9 @@
-g++ -o wyl wyl_old.cpp
-./wyl 10 IDW_10.txt /home/wcq/Documents/WYL/data/IDW/x_IDW.txt /home/wcq/Documents/WYL/data/IDW/y_IDW.txt /home/wcq/Documents/WYL/data/IDW/z_IDW.txt
-./wyl 20 IDW_20.txt /home/wcq/Documents/WYL/data/IDW/x_IDW.txt /home/wcq/Documents/WYL/data/IDW/y_IDW.txt /home/wcq/Documents/WYL/data/IDW/z_IDW.txt
-./wyl 30 IDW_30.txt /home/wcq/Documents/WYL/data/IDW/x_IDW.txt /home/wcq/Documents/WYL/data/IDW/y_IDW.txt /home/wcq/Documents/WYL/data/IDW/z_IDW.txt
-./wyl 10 Krig_10.txt /home/wcq/Documents/WYL/data/Krig/x_krig.txt /home/wcq/Documents/WYL/data/Krig/y_krig.txt /home/wcq/Documents/WYL/data/Krig/z_krig.txt
-./wyl 20 Krig_20.txt /home/wcq/Documents/WYL/data/Krig/x_krig.txt /home/wcq/Documents/WYL/data/Krig/y_krig.txt /home/wcq/Documents/WYL/data/Krig/z_krig.txt
-./wyl 30 Krig_30.txt /home/wcq/Documents/WYL/data/Krig/x_krig.txt /home/wcq/Documents/WYL/data/Krig/y_krig.txt /home/wcq/Documents/WYL/data/Krig/z_krig.txt
-#./wyl 20 Krig_result.txt  /home/wcq/Documents/WYL/data/Krig/y_krig.txt /home/wcq/Documents/WYL/data/Krig/z_krig.txt
+#!/binin/bash
+g++ -o OSPS src/OSPS_old.cpp
+./OSPS 10 IDW_10.txt data/IDW/x_IDW.txt data/IDW/y_IDW.txt data/IDW/z_IDW.txt
+./OSPS 20 IDW_20.txt data/IDW/x_IDW.txt data/IDW/y_IDW.txt data/IDW/z_IDW.txt
+./OSPS 30 IDW_30.txt data/IDW/x_IDW.txt data/IDW/y_IDW.txt data/IDW/z_IDW.txt
+./OSPS 10 Krig_10.txt data/Krig/x_krig.txt data/Krig/y_krig.txt data/Krig/z_krig.txt
+./OSPS 20 Krig_20.txt data/Krig/x_krig.txt data/Krig/y_krig.txt data/Krig/z_krig.txt
+./OSPS 30 Krig_30.txt data/Krig/x_krig.txt data/Krig/y_krig.txt data/Krig/z_krig.txt
+
--- work_time.sh
+++ work_time.sh
@@ -1,10 +1,11 @@
-g++ -o wyl wyl_time.cpp
-./wyl 30 time1.txt /home/wcq/Documents/WYL/data/IDW/x_IDW.txt /home/wcq/Documents/WYL/data/IDW/y_IDW.txt /home/wcq/Documents/WYL/data/IDW/z_IDW.txt 3
-./wyl 30 time2.txt /home/wcq/Documents/WYL/data/IDW/x_IDW.txt /home/wcq/Documents/WYL/data/IDW/y_IDW.txt /home/wcq/Documents/WYL/data/IDW/z_IDW.txt 2
-./wyl 30 time3.txt /home/wcq/Documents/WYL/data/IDW/x_IDW.txt /home/wcq/Documents/WYL/data/IDW/y_IDW.txt /home/wcq/Documents/WYL/data/IDW/z_IDW.txt 1
-./wyl 30 time4.txt /home/wcq/Documents/WYL/data/IDW/x_IDW.txt /home/wcq/Documents/WYL/data/IDW/y_IDW.txt /home/wcq/Documents/WYL/data/IDW/z_IDW.txt 0
+#!/bin/bash
+g++ -o OSPS src/OSPS_time.cpp
+./OSPS 30 time1.txt data/IDW/x_IDW.txt data/IDW/y_IDW.txt data/IDW/z_IDW.txt 3
+./OSPS 30 time2.txt data/IDW/x_IDW.txt data/IDW/y_IDW.txt data/IDW/z_IDW.txt 2
+./OSPS 30 time3.txt data/IDW/x_IDW.txt data/IDW/y_IDW.txt data/IDW/z_IDW.txt 1
+./OSPS 30 time4.txt data/IDW/x_IDW.txt data/IDW/y_IDW.txt data/IDW/z_IDW.txt 0
 
-#./wyl 30 time1.txt /home/wcq/Documents/WYL/data/Krig/x_krig.txt /home/wcq/Documents/WYL/data/Krig/y_krig.txt /home/wcq/Documents/WYL/data/Krig/z_krig.txt 3
-#./wyl 30 time2.txt /home/wcq/Documents/WYL/data/Krig/x_krig.txt /home/wcq/Documents/WYL/data/Krig/y_krig.txt /home/wcq/Documents/WYL/data/Krig/z_krig.txt 2
-#./wyl 30 time3.txt /home/wcq/Documents/WYL/data/Krig/x_krig.txt /home/wcq/Documents/WYL/data/Krig/y_krig.txt /home/wcq/Documents/WYL/data/Krig/z_krig.txt 1
-#./wyl 30 time4.txt /home/wcq/Documents/WYL/data/Krig/x_krig.txt /home/wcq/Documents/WYL/data/Krig/y_krig.txt /home/wcq/Documents/WYL/data/Krig/z_krig.txt 0
+#./OSPS 30 time1.txt data/Krig/x_krig.txt data/Krig/y_krig.txt data/Krig/z_krig.txt 3
+#./OSPS 30 time2.txt data/Krig/x_krig.txt data/Krig/y_krig.txt data/Krig/z_krig.txt 2
+#./OSPS 30 time3.txt data/Krig/x_krig.txt data/Krig/y_krig.txt data/Krig/z_krig.txt 1
+#./OSPS 30 time4.txt data/Krig/x_krig.txt data/Krig/y_krig.txt data/Krig/z_krig.txt 0
